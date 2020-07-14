50ad9919dc03de40e6e14db2ccc68e222aeeaf38
Jennifer Glauche <mail@jglauche.de>
2020-07-14 18:54:00 +0200

    make an example of complex line that fails

--- lib/test_loader.rb
+++ lib/test_loader.rb
@@ -3,15 +3,23 @@ class TestLoader
 
   attr_accessor :available_locales
   attr_accessor :test_mode, :ignored_views
+  attr_accessor :test_files, :test_lines, :test_i, :test_suggestions
 
   def initialize
     @test_mode = true
     @ignored_views = []
     @available_locales = get_locales
+    @test_files = []
+    @test_lines = []
+    @test_i = []
+    @test_suggestions = []
   end
 
   def test_feedback(file, line, i, suggestion)
-    return [file, line, i, suggestion]
+    @test_files << file
+    @test_lines << line
+    @test_i << i
+    @test_suggestions << suggestion
   end
 end
 
---
+++ spec/purr18n_spec.rb
@@ -0,0 +1,27 @@
+describe Purr18n do
+  before do
+    Dir.chdir($exec_dir + "/spec/sample_app_rails6/")
+    @loader = TestLoader.new
+  end
+
+  it 'scans devise edit.html.haml file' do
+    @loader.scan_view('app/views/devise/registrations/edit.html.haml')
+    expect(@loader.test_i.size).to eq 7
+    expect(@loader.test_suggestions[0]).to eq "t('.edit', s: resource_name.to_s.humanize)"
+    expect(@loader.test_suggestions[1]).to eq "t('.currently_waiting_confirmation_for', s: resource.unconfirmed_email)"
+    # TODO: check if i18n key has brackets
+    expect(@loader.test_suggestions[2]).to eq "t('.leave_blank_if_you_dont_want_to_change_it')"
+    expect(@loader.test_suggestions[3]).to eq "t('.characters_minimum')"
+
+    expect(@loader.test_suggestions[4]).to eq "t('.we_need_your_current_password_to_confirm_your_changes')"
+
+    expect(@loader.test_suggestions[5]).to eq "t('.cancel_my_account')"
+
+    expect(@loader.test_suggestions[6]).to eq "t('.unhappy', s: button_to t('.cancel_my_account'), registration_path(resource_name), data: { confirm: t('.are_you_sure') }, method: :delete)"
+
+
+
+
+  end
+
+end
